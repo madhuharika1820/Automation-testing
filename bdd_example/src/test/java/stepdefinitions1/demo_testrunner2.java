f76678e35c95441cd6d2bf1bbaeaf25b6ed12173
labuser <labuser@172.31.46.155>
2026-01-30 07:08:34 +0000

    df

--- bdd_example/src/test/java/stepdefinitions1/demo_testrunner2.java
+++ bdd_example/src/test/java/stepdefinitions1/demo_testrunner2.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.*;
+
 @SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/feature1/google",
